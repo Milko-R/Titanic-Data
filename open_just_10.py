8e3f739a9657a360771695ac4fdc27ca6a2e60c4
Milko Rodríguez <mcesar.arellano@gmail.com>
2020-02-18 10:28:12 -0500

    Update open_just_10.py

--- open_just_10.py
+++ open_just_10.py
@@ -1,5 +1,5 @@
 import csv
-with open('titanic.csv') as csvfile:
+with open('titanic_csv.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for i,row in enumerate(reader):
         print(row['survived'], row['pclass'], row['name'], row['sex'], row['age'])
